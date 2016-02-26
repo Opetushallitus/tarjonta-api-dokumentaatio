e8c5746c5a01edbe56df06f67545bafa6244f5c5
Alexis Troberg <alexis.troberg@gofore.com>
2016-02-26 11:34:03 +0200

    Korkeakoulutuksen hakukohteen lisäys/muokkaus

--- korkeakoulutus.js
+++ korkeakoulutus.js
@@ -3,6 +3,7 @@ const config = require('./config')
 const unirest = require('unirest')
 const assert = require('assert')
 const upsertKoulutus = require('./lib/upsertKoulutus')
+const upsertHakukohde = require('./lib/upsertHakukohde')
 
 authenticate().then(casTicket => {
 
@@ -116,6 +117,96 @@ authenticate().then(casTicket => {
         upsertKoulutus(casTicket, koulutusDelta).then(response => {
             assert.equal('OK', response.body.status)
             assert.equal(koulutusDelta.suunniteltuKestoArvo, response.body.result.suunniteltuKestoArvo)
+
+            // Liitetään koulutus hakuun lisäämällä sille hakukohde
+            // Huom! Haku luodaan tarjonnan käyttöliittymän kautta ja sieltä voi poimia tunnisteen talteen!
+            const hakuOid = '1.2.246.562.29.10714450698'
+
+            const hakukohde = {
+                hakuOid: hakuOid,
+                tila: 'LUONNOS',
+                koulutukset: [ // hakukohteeseen liitettävät koulutukset
+                    {
+                        oid: koulutusDelta.oid // voi viitata Opintopolun sisäisellä tunnisteella
+                                               // tai vaihtoehtoisesti {uniqueExternalId: 'korkeakoulun oma tunniste'}
+                    }
+                ],
+                aloituspaikatLkm: 15,
+                ensikertalaistenAloituspaikat: 10,
+                hakukelpoisuusVaatimusKuvaukset: {
+                    kieli_fi: '<p>vapaa teksti</p>'
+                },
+                hakukelpoisuusvaatimusUris: [ // https://testi.virkailija.opintopolku.fi/koodisto-service/rest/codeelement/codes/pohjakoulutusvaatimuskorkeakoulut/1
+                    'pohjakoulutusvaatimuskorkeakoulut_104'
+                ],
+                hakukohteenLiitteet: [
+                    {
+                        kieliUri: 'kieli_fi',
+                        liitteenKuvaukset: {
+                            kieli_fi: '<p>vapaa teksti</p>'
+                        },
+                        liitteenNimi: 'Testiliite',
+                        liitteenToimitusOsoite: {
+                            osoiterivi1: 'PL 11110',
+                            postinumero: 'posti_00076',
+                            postitoimipaikka: 'AALTO'
+                        },
+                        toimitettavaMennessa: new Date().getTime()
+                    }
+                ],
+                hakukohteenNimet: {
+                    kieli_fi: 'Hum kand hakukohde LAOREM'
+                },
+                lisatiedot: {
+                    kieli_fi: '<p>Vapaa teksti</p>'
+                },
+                soraKuvaukset: {
+                    kieli_fi: '<p>vapaa teksti</p>'
+                },
+                valintaperusteKuvaukset: {
+                    kieli_fi: '<p>vapaa teksti</p>'
+                },
+                ylioppilastutkintoAntaaHakukelpoisuuden: true,
+                valintakokeet: [
+                    {
+                        kieliUri: 'kieli_fi',
+                        valintakoeAjankohtas: [
+                            {
+                                alkaa: new Date().getTime(),
+                                loppuu: new Date().getTime(),
+                                osoite: {
+                                    osoiterivi1: 'Testikatu',
+                                    postinumero: 'posti_00350',
+                                    postitoimipaikka: 'HELSINKI'
+                                }
+                            }
+                        ],
+                        valintakoeNimi: 'Testivalintakoe',
+                        valintakokeenKuvaus: {
+                            uri: 'kieli_fi',
+                            teksti: '<p>Valintakokeen kuvaus</p>'
+                        },
+                        teksti: '<p>Valintakokeen kuvaus</p>'
+                    }
+                ]
+            }
+
+            upsertHakukohde(casTicket, hakukohde).then(response => {
+                assert.equal('OK', response.body.status)
+
+                // muokataan tallennettua hakukohdetta
+                const hakukohdeDelta = {
+                    oid: response.body.result.oid,
+                    lisatiedot: {
+                        kieli_fi: '<p>Muokattu teksti..</p>'
+                    }
+                }
+                upsertHakukohde(casTicket, hakukohdeDelta).then(response => {
+                    assert.equal('OK', response.body.status)
+                    assert.deepEqual(hakukohdeDelta.lisatiedot, response.body.result.lisatiedot)
+                })
+            })
+
         })
     })
 
